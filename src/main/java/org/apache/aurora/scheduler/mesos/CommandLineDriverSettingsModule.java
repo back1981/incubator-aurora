3f747c77d9bc1e8fd542879c25031c00b4a8468f
jyang12 <jyang12@ebay.com>
2015-02-05 11:18:38 +0800

    add mesos role

--- src/main/java/org/apache/aurora/scheduler/mesos/CommandLineDriverSettingsModule.java
+++ src/main/java/org/apache/aurora/scheduler/mesos/CommandLineDriverSettingsModule.java
@@ -50,6 +50,10 @@ public class CommandLineDriverSettingsModule extends AbstractModule {
       help = "Address for the mesos master, can be a socket address or zookeeper path.")
   private static final Arg<String> MESOS_MASTER_ADDRESS = Arg.create();
 
+  @CmdLine(name = "mesos_role",
+  help = "role defined in mesos")
+  private static final Arg<String> MESOS_ROLE = Arg.create("*");
+  
   @VisibleForTesting
   static final String PRINCIPAL_KEY = "aurora_authentication_principal";
   @VisibleForTesting
@@ -110,6 +114,7 @@ public class CommandLineDriverSettingsModule extends AbstractModule {
   protected void configure() {
     FrameworkInfo frameworkInfo = FrameworkInfo.newBuilder()
         .setUser(EXECUTOR_USER.get())
+		.setRole(MESOS_ROLE)
         .setName(TWITTER_FRAMEWORK_NAME)
         .setCheckpoint(REQUIRE_SLAVE_CHECKPOINT.get())
         .setFailoverTimeout(FRAMEWORK_FAILOVER_TIMEOUT.get().as(Time.SECONDS))
